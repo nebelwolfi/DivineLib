e66716ecf6596f5a863ea24633b82d054295294d
Nebelwolfi <dr.backdooring@gmail.com>
2020-05-23 14:12:49 +0200

    Update main.lua
    sdsfdbf

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ DivineLib = {
     _URL         = "https://pixelbot.org/DivineLib/"
 }
 
-module
+--[[module
     .load("DivineLib", "net")
     .update(
         DivineLib._NAME,
@@ -17,12 +17,22 @@ module
                 core.reload()
             end
         end
-    )
-
+    )]]
+    
 setmetatable(DivineLib, {
     __index = function(_, b)
         return module.load("DivineLib", b)
     end
 })
+
+module.load("DivineLib", "graphics")
+local m = menu("DivineLib", "DivineLib")
+m:button("orb", "Enable Divine Orbwalker", "Load", function()
+    module.load("DivineLib", "orb")
+end)
+m:boolean("auto", "Automatically load Orbwalker", false)
+if (m.auto:get()) then
+    module.load("DivineLib", "orb")
+end
 
 return DivineLib
